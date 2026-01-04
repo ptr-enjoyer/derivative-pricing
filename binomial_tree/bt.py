79d64b95e0ca959bde8d0565cfdbeb94ea5fbc27
Leonardo Rotondi <leonardo.rotondi@gmail.com>
2026-01-04 18:38:26 +0000

    Built price_option method and updated notebook with the theory and the function

--- binomial_tree/bt.py
+++ binomial_tree/bt.py
@@ -4,13 +4,20 @@ import matplotlib.pyplot as plt
 
 class BinomialTree():
 
-    def __init__(self, E, T, u, d, contract='European'):
+    def __init__(self, E, T, u, d, steps, contract='European'):
         self.E = E
         self.T = T
         self.u = u
         self.d = d
+        self.steps = steps
         self.contract = contract
     
     def calc_delta(self, V_p, V_m, S):
         delta = (V_p - V_m) / (self.u*S - self.d*S)
-        return delta 
+        return delta
+    
+    def option_val(self, r, V_p, V_m):
+        t = self.T / self.steps
+        p = (np.exp ** (r*t) - self.d) / (self.u-self.d)
+        V = np.exp **(-r*t) * (p * V_p + (1-p)* V_m)
+        return V
