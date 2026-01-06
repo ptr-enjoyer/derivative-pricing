c44737de27cc5d8b66bb99d6d1b5128f0792ab72
Leonardo Rotondi <leonardo.rotondi@gmail.com>
2026-01-06 21:25:53 +0000

    Added stock_price method. Will move onto evaluationg the option

--- binomial_tree/bt.py
+++ binomial_tree/bt.py
@@ -4,11 +4,12 @@ import matplotlib.pyplot as plt
 
 class BinomialTree():
 
-    def __init__(self, E, T, u, d, steps, contract='European'):
+    def __init__(self, E, T, u, d, S_0, steps, contract='European'):
         self.E = E
         self.T = T
         self.u = u
         self.d = d
+        self.S_0 = S_0
         self.steps = steps
         self.contract = contract
     
@@ -21,3 +22,12 @@ class BinomialTree():
         p = (np.exp ** (r*t) - self.d) / (self.u-self.d)
         V = np.exp **(-r*t) * (p * V_p + (1-p)* V_m)
         return V
+    
+    def stock_price(self):
+        S = []
+        for i in range(self.steps):
+            depth = []
+            for j in range(i):
+                depth.append(self.S_0 * (self.u**j ) * (self.d**(i-j)))
+            S.append(depth)
+        return S
